fc040667b20b12c52fb42dfdfb687a03a0c0601d
mwaitukasteven <mwaitukasteven@gmail.com>
2026-01-20 02:06:43 +0300

    learning react day 4

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Great from './components/greet';
 import Welcome from './components/welcome'; 
+import Home from './components/home';
 
 function App() {
   return (
     <div className="App">
       <Great />
       <Welcome />
+      <Home />
     </div>
   );
 }
