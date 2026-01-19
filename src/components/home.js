5b0dd9ce05522020f4b8849ccd167f2c520c87c5
mwaitukasteven <mwaitukasteven@gmail.com>
2026-01-20 02:11:22 +0300

    learning react day 4

--- src/components/home.js
+++ src/components/home.js
@@ -2,7 +2,7 @@ import react from 'react';
 
 const Home = () => {
     return React.createElement('div', 
-        {id: 'home'}, 
+        {id: 'home', className: 'home-class'}, 
         React.createElement('h1', null, 'This is the Home Component')
         );
 
